02a1410ee5354df17825f9bbd17b917121f35122
Sergey Grin <grinzzly@gmail.com>
2019-11-26 01:26:48 +0100

    Provide face recognition models

--- src/modules/Heart/Heart.js
+++ src/modules/Heart/Heart.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import { Jumbotron } from 'react-bootstrap';
 
+import { nets } from 'face-api.js';
+
 import { VideoOutput } from '../VideoOutput';
 
 import './Heart.less';
@@ -13,7 +15,8 @@ export class Heart extends Component {
       video: null,
     };
 
-    this.getVideo();
+    this.initFaceApi()
+      .then(this.getVideo());
   }
 
   async getVideo() {
@@ -25,11 +28,20 @@ export class Heart extends Component {
     this.setState({ video });
   }
 
+  initFaceApi() {
+    return Promise.all([
+      nets.tinyFaceDetector.loadFromUri('/models'),
+      nets.faceLandmark68Net.loadFromUri('/models'),
+      nets.faceRecognitionNet.loadFromUri('/models'),
+      nets.faceExpressionNet.loadFromUri('/models'),
+    ]);
+  }
+
   render() {
     const { video } = this.state;
 
     return (
-      <Jumbotron className="Heart">
+      <Jumbotron id="heart" className="Heart">
         {video ? <VideoOutput video={video} /> : ''}
       </Jumbotron>
     );
--- src/modules/VideoOutput/VideoOutput.js
+++ src/modules/VideoOutput/VideoOutput.js
@@ -2,6 +2,16 @@ import React, { Component, createRef } from 'react';
 
 import PropTypes from 'prop-types';
 
+import {
+  draw,
+  TinyFaceDetectorOptions,
+  matchDimensions,
+  detectAllFaces,
+  resizeResults,
+  createCanvasFromMedia,
+  isMediaLoaded
+} from 'face-api.js';
+
 import './VideoOutput.less';
 
 
@@ -29,10 +39,34 @@ export class VideoOutput extends Component {
     const { video } = this.props;
 
     videoObj.srcObject = video;
-    videoObj.play();
+    videoObj.play()
+      .then(videoObj.addEventListener('play', this.initCanvas(videoObj), false));
   }
 
+  initCanvas = (video) => {
+    setTimeout(() => {
+      if (isMediaLoaded(video)) {
+        const canvas = createCanvasFromMedia(video);
+        document.getElementById('heart').append(canvas);
+        const displaySize = { width: video.width, height: video.height };
+        matchDimensions(canvas, displaySize);
+
+        setInterval(async () => {
+          const detections = await detectAllFaces(
+            video,
+            new TinyFaceDetectorOptions(),
+          ).withFaceLandmarks().withFaceExpressions();
+          const resizedDetections = resizeResults(detections, displaySize);
+          canvas.getContext('2d').clearRect(0, 0, canvas.width, canvas.height);
+          draw.drawDetections(canvas, resizedDetections);
+          draw.drawFaceLandmarks(canvas, resizedDetections);
+          draw.drawFaceExpressions(canvas, resizedDetections);
+        }, 100);
+      }
+    }, 1000);
+  };
+
   render() {
-    return <video id="video" width="720" height="560" autoPlay muted ref={this.videoRef} />;
+    return <video width="720" height="560" autoPlay muted ref={this.videoRef} />;
   }
 }
